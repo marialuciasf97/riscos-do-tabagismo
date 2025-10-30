f60c13c7ae80217d01a7dddeb1f3c503e4f36e80
marialuciasf97 <marialuciasf97@gmail.com>
2025-10-30 03:35:50 -0300

    Adiciona recodegenerator

---
+++ qrcodegenerator/src/main/java/com/marialuciasf97/qrcodegenerator/QrcodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.marialuciasf97.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodegeneratorApplication.class, args);
+	}
+
+}
